a7e5248eb1c8d7e6ae5df1d9fcaac14ada2d7199
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-06-02 14:13:01 -0300

    Começando a tela de cadastro.

--- app/src/main/java/br/com/spendingcontrol/activities/RegisterActivity.java
+++ app/src/main/java/br/com/spendingcontrol/activities/RegisterActivity.java
@@ -3,14 +3,36 @@ package br.com.spendingcontrol.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.com.spendingcontrol.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText etEmail;
+    EditText etPassword;
+    EditText etConfirmPassword;
+
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
